9b38dde11cc1a5492b5fda8b0b0351762fd882b0
ingyu1008 <ingyu1008@gmail.com>
2021-05-01 17:19:54 +0900

    ADD : Codeforces Graph

---
+++ CodeforcesGraph.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('ignore-certificate-errors')
+options.add_argument('headless')
+driver = webdriver.Chrome(executable_path='./chromedriver', options=options)
+
+URL = 'https://codeforces.com/profile/MatWhyTle?graphType=rated'
+
+driver.get(URL)
+
+sleep(1)
+
+driver.find_element_by_id('usersRatingGraphPlaceholder').screenshot('graph.png')
+
+driver.quit()
